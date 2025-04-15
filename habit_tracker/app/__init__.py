3ccdc6fab181b72b30596a66ce799691834bbc1f
ss <saltsoda7@gmail.com>
2025-04-15 16:41:00 +0800

    Modified the init file

--- habit_tracker/app/__init__.py
+++ habit_tracker/app/__init__.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'static'))
 
 # You can configure your app here
-app.config['SECRET_KEY'] = 'your_secret_key'
+# app.config['SECRET_KEY'] = 'your_secret_key'
 
 # Import your routes after app is initialized
 from app import routes
