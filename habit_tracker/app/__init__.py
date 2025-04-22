5b26d68a76a4f467362f78b73353523e114ab7e2
ss <saltsoda7@gmail.com>
2025-04-23 01:33:16 +0800

    Add yearly.html

--- habit_tracker/app/__init__.py
+++ habit_tracker/app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the Flask app
-app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'static'))
+# Initialize the SQLAlchemy
+db = SQLAlchemy()
 
-# You can configure your app here
-# app.config['SECRET_KEY'] = 'your_secret_key'
+def create_app():
+    # Initialize the Flask app
+    app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'static'))
+    
+    # Configure the app
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///habit_tracker.db'  
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+    # app.config['SECRET_KEY'] = 'your_secret_key'  
+    
+    # Initialize SQLAlchemy with the app
+    db.init_app(app)  
+    
+    # Import your routes after app is initialized
+    from app import routes
 
-# Import your routes after app is initialized
-from app import routes
+    return app
