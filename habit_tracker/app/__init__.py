000daaf2dff4ac330405740b904f13b9662504b6
Fiona Wei <wyf25@hotmail.com>
2025-04-23 21:50:31 +0800

    Added database

--- habit_tracker/app/__init__.py
+++ habit_tracker/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
 import os
 
-# Initialize the Flask app
 app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'static'))
+app.config.from_object(Config)   # Load the configuration from the Config class
+db = SQLAlchemy(app)             # Initialize the database
+migrate = Migrate(app, db)       # Initialize the migration tool
 
-# You can configure your app here
-# app.config['SECRET_KEY'] = 'your_secret_key'
-
-# Import your routes after app is initialized
-from app import routes
+from app import routes, models
