f8eeff81eab7f086f5cad78e5a58dc8af356a65d
Fiona <167882976+fifibox@users.noreply.github.com>
2025-06-14 22:27:24 +0800

    Update routes.py

--- habit_tracker/app/auth/routes.py
+++ habit_tracker/app/auth/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, current_user
-from app import db, login_manager
-from app.models import User   # example model
+from habit_tracker.app import db, login_manager
+from habit_tracker.app.models import User   # example model
 from . import auth_bp
 import re
 
